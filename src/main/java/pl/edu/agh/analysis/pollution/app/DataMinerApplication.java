d79d3cf57c3247e82dce6dfbe29a01c6a946316d
wzagrajcz <wzagrajcz@gmail.com>
2017-10-19 21:46:53 +0200

    DEFSC-17 Extending logs

--- src/main/java/pl/edu/agh/analysis/pollution/app/DataMinerApplication.java
+++ src/main/java/pl/edu/agh/analysis/pollution/app/DataMinerApplication.java
@@ -1,13 +1,17 @@
 package pl.edu.agh.analysis.pollution.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DataMinerApplication {
+    private final static Logger LOGGER = LoggerFactory.getLogger("pl.edu.agh.analysis.pollution.app.DataMinerApplication");
 
     public static void main(String[] args) {
+        LOGGER.info("Starting application with arguments %s", args);
         SpringApplication app = new SpringApplication(DataMinerApplication.class);
         app.setWebEnvironment(false);
         ConfigurableApplicationContext ctx = app.run(args);
